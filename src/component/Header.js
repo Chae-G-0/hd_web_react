17ab8255a1e709eb40feae7920c8cbb35382a4a8
chae <corkdud@naver.com>
2022-09-14 15:29:28 +0900

    220914

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TopBanner from "./TopBanner";
 
 const NAVLINK = [
@@ -10,45 +10,67 @@ const NAVLINK = [
 ];
 
 const Header = () => {
-    const [TG, setTG] = useState(false)
-    const [TS, setTS] = useState(false)
+    const [TG, setTG] = useState(false);
+    const HL = useRef();
+    const [TS, setTS] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            let sct = window.scrollY;
+            sct > 0 
+                ? HL.current.classList.add("on") 
+                : HL.current.classList.remove("on");
+        });
+    }, []);
     return (
-        <header className="Header">
-          <TopBanner/>
+        <header className="Header" ref={HL}>
+            <TopBanner />
             <div className="hd_wrap">
-            <h1>
-                <a href="/">현대엘리베이터</a>
-            </h1>
-            <nav className="Gnb inner">
-                <ul>
-                    {NAVLINK.map((it, idx) => {
-                        return (
-                            <li key={it.id}>
-                                <a href={it.link}>{it.menu}</a>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </nav>
-            <div className="top_service">
-                    <ul className="project">
-                        <li><a href="#!">주요프로젝트</a></li>
-                        <li><a href="#!">IR</a></li>
+                <h1>
+                    <a href="/">현대엘리베이터</a>
+                </h1>
+                <nav className="Gnb inner">
+                    <ul>
+                        {NAVLINK.map((it, idx) => {
+                            return (
+                                <li key={it.id}>
+                                    <a href={it.link}>{it.menu}</a>
+                                </li>
+                            );
+                        })}
                     </ul>
-                    <div className={'lang ' + (TG ? 'on' : '') }>
-                        <strong onClick={()=> setTG(!TG)}><i className="xi-globus"></i> KOR <i className="xi-angle-down arrow"></i></strong>
+                </nav>
+                <div className="top_service">
+                    <ul className="project">
+                        <li>
+                            <a href="#!">주요프로젝트</a>
+                        </li>
+                        <li>
+                            <a href="#!">IR</a>
+                        </li>
+                    </ul>
+                    <div className={"lang " + (TG ? "on" : "")}>
+                        <strong onClick={() => setTG(!TG)}>
+                            <i className="xi-globus"></i> KOR <i className="xi-angle-down arrow"></i>
+                        </strong>
                         <ul className="lang_box">
-                            <li><a href="#!">KOR</a></li>
-                            <li><a href="#!">ENG</a></li>
-                            <li><a href="#!">CHN</a></li>
+                            <li>
+                                <a href="#!">KOR</a>
+                            </li>
+                            <li>
+                                <a href="#!">ENG</a>
+                            </li>
+                            <li>
+                                <a href="#!">CHN</a>
+                            </li>
                         </ul>
                     </div>
-                    <div className={`top_search ${TS ? 'on' : ''}`} >
-                        <strong onClick={()=> setTS(!TS)}><i className="xi-search"></i></strong>
+                    <div className={`top_search ${TS ? "on" : ""}`}>
+                        <strong onClick={() => setTS(!TS)}>
+                            <i className="xi-search"></i>
+                        </strong>
                         <div className="search_box">
-
                             <form action="#!">
-                                <input type="text" placeholder="검색어를 입력하세요" required/>
+                                <input type="text" placeholder="검색어를 입력하세요" required />
                                 <button>
                                     <i className="xi-search"></i>
                                 </button>
--- src/component/MainVisual.js
+++ src/component/MainVisual.js
@@ -3,9 +3,29 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 
 const SLIDE = [
-    { id: 1, content: "01 The world expands", desc: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다.", link: "https://my.matterport.com/show/?m=yEdf9FWaNav" },
-    { id: 2, content: "02 The world expands", desc: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다.", link: "https://my.matterport.com/show/?m=yEdf9FWaNav" },
-    { id: 3, content: "03 The world expands", desc: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다.", link: "https://my.matterport.com/show/?m=yEdf9FWaNav" },
+    {
+        id: 1,
+        content: 
+                "01 The world expands along\nthe new pach created",
+
+        desc: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다.",
+        link: "https://my.matterport.com/show/?m=yEdf9FWaNav",
+        title: "The world expands",
+    },
+    {
+        id: 2,
+        content: "02 The world expands along the\n new pach created",
+        desc: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다.",
+        link: "https://my.matterport.com/show/?m=yEdf9FWaNav",
+        title: "New pach created",
+    },
+    {
+        id: 3,
+        content: "03 The world expands\n along the new pach created",
+        desc: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다.",
+        link: "https://my.matterport.com/show/?m=yEdf9FWaNav",
+        title: "The world expands",
+    },
 ];
 
 const MainVisual = () => {
@@ -29,9 +49,9 @@ const MainVisual = () => {
                     return (
                         <figure key={slide.id} className={"itm0" + slide.id + (idx === IDX ? " on" : "")}>
                             <div className="inner">
-                                <h2>{slide.content}</h2>
-                                <p>{slide.desc}</p>
-                                <a href={slide.link} target="_blank">
+                                <p className="tit">{slide.content}</p>
+                                <div className="des">{slide.desc}</div>
+                                <a href={slide.link} target="_blank" className="cbtn">
                                     VIEW MORE
                                 </a>
                             </div>
@@ -46,17 +66,17 @@ const MainVisual = () => {
             <ul className="slideTab">
                 {SLIDE.map((dots, idx) => {
                     return (
-                        <li key={dots.id} className={idx === IDX ? " on" : ""}>
-                            {dots.content}
+                        <li key={dots.id} className={idx === IDX ? " on" : ""} onClick={()=> mainSlide.current.slickGoTo(idx)}>
+                            {dots.title}
                         </li>
                     );
                 })}
             </ul>
             <div className="slideArrows">
-                <button onClick={()=>mainSlide.current.slickPrev()}>
+                <button onClick={() => mainSlide.current.slickPrev()}>
                     <i className="xi-arrow-left"></i>
                 </button>
-                <button  onClick={()=>mainSlide.current.slickNext()}>
+                <button onClick={() => mainSlide.current.slickNext()}>
                     <i className="xi-arrow-right"></i>
                 </button>
             </div>
